cecfd66d129375b903a066663d9af0df1e4afa1b
Elijah Bigham <Elijahbigham@gmail.com>
2020-01-14 15:19:34 -0500

    0.1.0 — Category choice screen is not on main storyboard.

--- UpsetTheAppleTree/ViewController.swift
+++ UpsetTheAppleTree/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var choice = -1
+    //Indictates which option the user has selected
+    //Animal = 0, Food = 1, Sports = 2, Celestial Bodies = 3
+    @IBOutlet weak var chooseAButton: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func AnimalButton(_ sender: Any) {
+        choice = 0
+    }
+    
+    @IBAction func FoodButton(_ sender: Any) {
+        choice = 1
+    }
+    @IBAction func SportsButton(_ sender: Any) {
+        choice = 2
+    }
+    @IBAction func CelestialBodiesButton(_ sender: Any) {
+        choice = 3
+    }
 }
 
