b527fd7a20c1332f272d3d50eea13ec1e01898d7
DrSpyse <juliangaming2.0@gmail.com>
2020-01-16 16:36:01 -0500

    Did some crazy shit
    made this complicated nested for loop
    thingy that absolutely fried my brain
    but it ended up working. Fuck swift.

--- UpsetTheAppleTree/treeViewController.swift
+++ UpsetTheAppleTree/treeViewController.swift
@@ -38,6 +38,36 @@ class treeViewController: UIViewController {
     @IBOutlet weak var apple5: UIImageView!
     @IBOutlet weak var apple6: UIImageView!
     @IBOutlet weak var apple7: UIImageView!
+    //END OF APPLES
+    
+    //Buttons
+    @IBOutlet weak var _A: UIButton!
+    @IBOutlet weak var _B: UIButton!
+    @IBOutlet weak var _C: UIButton!
+    @IBOutlet weak var _D: UIButton!
+    @IBOutlet weak var _E: UIButton!
+    @IBOutlet weak var _F: UIButton!
+    @IBOutlet weak var _G: UIButton!
+    @IBOutlet weak var _H: UIButton!
+    @IBOutlet weak var _I: UIButton!
+    @IBOutlet weak var _J: UIButton!
+    @IBOutlet weak var _K: UIButton!
+    @IBOutlet weak var _L: UIButton!
+    @IBOutlet weak var _M: UIButton!
+    @IBOutlet weak var _N: UIButton!
+    @IBOutlet weak var _O: UIButton!
+    @IBOutlet weak var _P: UIButton!
+    @IBOutlet weak var _Q: UIButton!
+    @IBOutlet weak var _R: UIButton!
+    @IBOutlet weak var _S: UIButton!
+    @IBOutlet weak var _T: UIButton!
+    @IBOutlet weak var _U: UIButton!
+    @IBOutlet weak var _V: UIButton!
+    @IBOutlet weak var _W: UIButton!
+    @IBOutlet weak var _X: UIButton!
+    @IBOutlet weak var _Y: UIButton!
+    @IBOutlet weak var _Z: UIButton!
+    //END OF BUTTONS
     
     //This is the correct answer
     var answer = "HELLO WORLD"
@@ -101,7 +131,7 @@ class treeViewController: UIViewController {
     }
     
     //Called on each button click. Makes shit happen.
-    func Update(targetLetter : Character){
+    func Update(pressedLetter : Character){
         for i in 0..<answer.count{
             
             //This is for finding if the letter occurs in the string
@@ -109,7 +139,7 @@ class treeViewController: UIViewController {
             let currentLetter = answer[currentLetterIndex]
             print(currentLetter)
             
-            if (targetLetter == currentLetter){
+            if (pressedLetter == currentLetter){
                 letterInWord = true
             }
         }
@@ -117,7 +147,9 @@ class treeViewController: UIViewController {
         if(letterInWord){
             //Make letter appear
             print("Letter is in word")
-            addLetters(letter: targetLetter)
+            addLetters(letter: pressedLetter)
+            
+            //Resets letter in word for the next choice
             letterInWord = false
         } else {
             //Make apple dissappear
@@ -134,20 +166,94 @@ class treeViewController: UIViewController {
             print(currentLetter)
             
             if (letter == currentLetter){
-                letterInWord = true
+                
+                let indexToChange = labelText.index(labelText.startIndex, offsetBy: i * 2)
+                
+                //Will become an exact replica of label text with the new letters added
+                var newLabelText = ""
+                
+                //This for loop adds labelText to newLabelText character by character until it gets to the index to change in which case it will add the letter of the button pressed. This gives me a headache.
+                for x in 0..<labelText.count{
+                    
+                    let nestedCurrentLetterIndex = labelText.index(labelText.startIndex, offsetBy: x)
+                    let nestedCurrentLetter = labelText[nestedCurrentLetterIndex]
+                    
+                    if(nestedCurrentLetterIndex != indexToChange){
+                        newLabelText += String(nestedCurrentLetter)
+                    } else {
+                        newLabelText += String(letter)
+                    }
+                }
+                //This is where it sets labelText to the finished newLabelText
+                labelText = newLabelText
+                //And then sets the text to the UI_Label to labelText
+                textLabel.text = labelText
+                
+                //HOLY SHIT IT ACTUALLY WORKS!!!!!
             }
         }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //This checks what button is pressed and acts accordingly
+    @IBAction func buttonPress(sender: UIButton){
+        switch sender.currentTitle{
+        case "A":
+            Update(pressedLetter: "A")
+        case "B":
+            Update(pressedLetter: "B")
+        case "C":
+            Update(pressedLetter: "C")
+        case "D":
+            Update(pressedLetter: "D")
+        case "E":
+            Update(pressedLetter: "E")
+        case "F":
+            Update(pressedLetter: "F")
+        case "G":
+            Update(pressedLetter: "G")
+        case "H":
+            Update(pressedLetter: "H")
+        case "I":
+            Update(pressedLetter: "I")
+        case "J":
+            Update(pressedLetter: "J")
+        case "K":
+            Update(pressedLetter: "K")
+        case "L":
+            Update(pressedLetter: "L")
+        case "M":
+            Update(pressedLetter: "M")
+        case "N":
+            Update(pressedLetter: "N")
+        case "O":
+            Update(pressedLetter: "O")
+        case "P":
+            Update(pressedLetter: "P")
+        case "Q":
+            Update(pressedLetter: "Q")
+        case "R":
+            Update(pressedLetter: "R")
+        case "S":
+            Update(pressedLetter: "S")
+        case "T":
+            Update(pressedLetter: "T")
+        case "U":
+            Update(pressedLetter: "U")
+        case "V":
+            Update(pressedLetter: "V")
+        case "W":
+            Update(pressedLetter: "W")
+        case "X":
+            Update(pressedLetter: "X")
+        case "Y":
+            Update(pressedLetter: "Y")
+        case "Z":
+            Update(pressedLetter: "Z")
+        default:
+            print("Something bad happened")
+        }
     }
-    */
-
+    
+    
+    
 }
